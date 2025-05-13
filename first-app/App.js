c9c0f96c867c3f910821981b3fe813bcf80d6f9c
Aditya Bhattad <adityabhattad18@gmail.com>
2025-05-13 22:20:32 +0530

    feat: working goal state

--- first-app/App.js
+++ first-app/App.js
@@ -1,18 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput,Button ,SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, SafeAreaView } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [goal, setGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  function handleInputChange(text) {
+    setGoal(text);
+  }
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => [...prevGoals, goal]);
+    setGoal('');
+  }
+
+  function handleDeleteGoal(index) {
+    setGoals((prevGoals) => prevGoals.filter((_, i) => i !== index));
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder='Write your goal here'
+          onChangeText={handleInputChange}
+          value={goal}
         />
         <Button
           title='Submit'
+          onPress={handleAddGoal}
         />
       </View>
-      <Text>I love react native!</Text>
+      <View style={styles.goalsList}>
+        {goals.length === 0 && <Text>No goals added yet!</Text>}
+        {goals.map((goal, index) => (
+          <View key={index} style={styles.goalText}>
+            <Text >
+              {goal}
+            </Text>
+            <Button
+              title='Delete'
+              onPress={() => handleDeleteGoal(index)}
+            />
+          </View>
+        ))}
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -25,7 +59,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'start',
   },
-  inputContainer:{
+  inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -34,5 +68,18 @@ const styles = StyleSheet.create({
     borderBottomColor: '#ccc',
     borderBottomWidth: 1,
     padding: 16,
-  }
+  },
+  goalsList: {
+    width: '80%',
+  },
+  goalText: {
+    padding: 8,
+    marginVertical: 4,
+    backgroundColor: '#5e0f7',
+    color: 'white',
+    borderRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 });
