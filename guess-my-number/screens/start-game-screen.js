03fa48037a492d092cdd7d9e3a93a2a1df089ef0
Aditya Bhattad <adityabhattad18@gmail.com>
2025-05-22 18:51:52 +0530

    feat: add styles to the input container for start game screen

--- guess-my-number/screens/start-game-screen.js
+++ guess-my-number/screens/start-game-screen.js
@@ -1,21 +1,44 @@
 import {
-    TextInput
+    View,
+    TextInput,
+    StyleSheet,
 } from "react-native";
 import PrimaryButton from "../components/primary-button";
 
 
-export default function StartGameScreen(){
+export default function StartGameScreen() {
     return (
-        <View>
+        <View style={styles.container}>
             <TextInput
                 placeholder="Enter the number here."
             />
-            <PrimaryButton>
-                Confim 
-            </PrimaryButton>
-            <PrimaryButton>
-                Reset
-            </PrimaryButton>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton>
+                    Confim
+                </PrimaryButton>
+                <PrimaryButton>
+                    Reset
+                </PrimaryButton>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 32,
+        marginVertical:300,
+        marginHorizontal:50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:'#58085b',
+        borderRadius:8,
+        elevation:12,
+    },
+    buttonContainer:{
+        display:'flex',
+        flexDirection:'row',
+        gap:8
+    }
+})
